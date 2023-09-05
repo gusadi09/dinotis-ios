8b241df587a7f0bf8d039da9b457e19cd38598b1
Agus Adi <gusadi872@gmail.com>
2023-09-05 16:35:57 +0700

    [FIX] delete redeclaration function and merge request

--- DinotisApp/UI/View/Home/ViewModel/TalentHomeViewModel.swift
+++ DinotisApp/UI/View/Home/ViewModel/TalentHomeViewModel.swift
@@ -216,18 +216,6 @@ final class TalentHomeViewModel: ObservableObject {
         }
     }
     
-    func getBottomSafeArea() -> CGFloat{
-        let keyWindow = UIApplication.shared.connectedScenes
-            .filter({ $0.activationState == .foregroundActive })
-            .map({ $0 as? UIWindowScene })
-            .compactMap({ $0 })
-            .first?.windows
-            .filter({ $0.isKeyWindow }).first
-        
-        return keyWindow?.safeAreaInsets.bottom ?? 0
-        
-    }
-    
     func getCounter() async {
         onStartedFetch()
         
